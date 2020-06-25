646e018f1474fec3966d7a113779e03fd8601adf
Bob Pham <bobpham.tdp@live.com>
2020-06-25 19:00:17 +1000

    work in progress

---
+++ Tdp.GeospatialConverter.Specs/GeoConvertingService.feature
@@ -0,0 +1,10 @@
+﻿Feature: GeoConvertingService
+	In order to use the page
+	As a normal user
+	I want to access the page's functions
+
+@mytag
+Scenario: Show the correct page title
+	Given The page url	
+	When I access the page
+	Then the result should be "Geospatial Data Converter" on the screen
---
+++ Tdp.GeospatialConverter.Specs/GeoConvertingServiceSteps.cs
@@ -0,0 +1,35 @@
+﻿using FluentAssertions;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using TechTalk.SpecFlow;
+
+namespace Tdp.GeospatialConverter.Specs
+{
+    [Binding]
+    public class GeoConvertingServiceSteps
+    {
+        private string _url;
+        private IWebDriver _driver;
+
+        [Given(@"The page url")]
+        public void GivenThePageUrl()
+        {
+            _url = "https://tdp-techground.info/GeoConvertingService";
+
+            _driver = new ChromeDriver();
+        }
+
+        [When(@"I access the page")]
+        public void WhenIAccessThePage()
+        {
+            _driver.Navigate().GoToUrl(_url);
+        }
+
+        [Then(@"the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBeOnTheScreen(int p0)
+        {
+            var title = _driver.Title;
+            title.Should().Be("Geospatial Data Converter");
+        }
+    }
+}
