0427c62f5cee17231dfd22f5dd50e2eb2a4d12ea
Bob Pham <bobpham.tdp@live.com>
2020-06-30 22:19:18 +1000

    add build number

--- Tdp.GeospatialConverter.Svc/Config/GeoServiceConfiguration.cs
+++ Tdp.GeospatialConverter.Svc/Config/GeoServiceConfiguration.cs
@@ -1,14 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace Tdp.GeospatialConverter.Svc.Config
+﻿namespace Tdp.GeospatialConverter.Svc.Config
 {
     public class GeoServiceConfiguration
     {
         public string SavedDataPath { get; set; }
 
         public string LocalDataPath { get; set; }
+
+        public string BuildNumber { get; set; }
     }
 }
--- Tdp.GeospatialConverter.Svc/Controllers/HomeController.cs
+++ Tdp.GeospatialConverter.Svc/Controllers/HomeController.cs
@@ -1,12 +1,15 @@
 ﻿using System.Web.Mvc;
+using Tdp.GeospatialConverter.Svc.Config;
 
 namespace Tdp.GeospatialConverter.Svc.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: Home
-        public ActionResult Index()
+      // GET: Home
+        public ActionResult Index(GeoServiceConfiguration serviceConfiguration)
         {
+            ViewBag.BuildNumber = serviceConfiguration.BuildNumber;
+
             return View();
         }
     }
